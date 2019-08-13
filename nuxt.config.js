436dd483387139083fe963067aa9c1d5d9210d8f
Shinnosuke Mogi <sin0225skyhigh@icloud.com>
2019-08-13 20:14:41 +0900

    ページタイトルの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'shinnopo chronicle',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -40,7 +40,7 @@ export default {
     '@nuxtjs/font-awesome'
   ],
   manifest: {
-    name: 'shinnopo chronicle',
+    name: 'shinnopo_nuxt_portfolio',
     lang: 'ja'
   },
 
