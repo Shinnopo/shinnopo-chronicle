c018b171502d1196ad8d6e5d9081f6517a135161
Shinnosuke Mogi <sin0225skyhigh@icloud.com>
2020-07-26 16:42:39 +0900

    change color

--- pages/culture.vue
+++ pages/culture.vue
@@ -66,7 +66,6 @@ a {
   display: block;
   padding: 20px 15px;
   text-align: center;
-  font-weight: bold;
   text-decoration: none;
   color: #fff;
 }
@@ -85,7 +84,7 @@ iframe {
     width: 100vw;
     height: 200px;
     margin: 0 auto;
-    background-color: #333;
+    background-color: #111;
   }
 
 .text-copy {
@@ -98,27 +97,27 @@ iframe {
 }
 
 .text-copy:nth-child(1){
-    stroke: #3399FF;
+    stroke: #0cdce0;
     animation-delay: -1;
 }
 
 .text-copy:nth-child(2){
-    stroke: #000099;
+    stroke: #0cdce0;
     animation-delay: -2s;
 }
 
 .text-copy:nth-child(3){
-    stroke: #000000;
+    stroke: #0cdce0;
     animation-delay: -3s;
 }
 
 .text-copy:nth-child(4){
-    stroke: #00FF7F;
+    stroke: #0cdce0;
     animation-delay: -4s;
 }
 
 .text-copy:nth-child(5){
-    stroke: #3CB371;
+    stroke: #0cdce0;
     animation-delay: -5s;
 }
 
